62cd867f9b2bb2a65874477efb9d45f2b636d72d
David Svantesson <pelleitorp2@gmail.com>
2023-02-01 10:01:43 +0100

    Initial commit

---
+++ src/main/java/se/lexicon/assingment_group_prodject/AssingmentGroupProdjectApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.assingment_group_prodject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssingmentGroupProdjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssingmentGroupProdjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/lexicon/assingment_group_prodject/AssingmentGroupProdjectApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.assingment_group_prodject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssingmentGroupProdjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
